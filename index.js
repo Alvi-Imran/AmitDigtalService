3c9df445879253433c087e879dfac07ad2234b70
alvi-imran <teachmind0007@gmail.com>
2025-10-18 21:26:09 +0530

    make slider

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ function updateServiceBox() {
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
+    autoplay:{
+        delay:2500,
+        disableOnInteraction:false
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,

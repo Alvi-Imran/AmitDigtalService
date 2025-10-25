e76a6187a18182a08c729a3b2dca40df3adf4db9
alvi-imran <teachmind0007@gmail.com>
2025-10-25 22:56:39 +0530

    custoer rivew 500px

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ function leftbtn() {
     index--
     if (index < 0) {
         index = serviceBox.length - 1;
-    }    
+    }
     updateServiceBox()
 }
 
-setInterval(()=>{
+setInterval(() => {
     rightbtn();
-},1500);
+}, 1500);
 
-function updateServiceBox() {    
+function updateServiceBox() {
     serviceCover.style.transform = `translateX(${-index * boxWidth}px)`;
 }
 
@@ -33,14 +33,32 @@ function updateServiceBox() {
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
-    autoplay:{
-        delay:2500,
-        disableOnInteraction:false
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
     },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
+    breakpoints: {
+        // When screen width is >= 320px (Mobile)
+        0: {
+            slidesPerView: 1,
+        },
+        // When screen width is >= 640px (Tablet)
+        640: {
+            slidesPerView: 2,
+        },
+        // When screen width is >= 1024px (Laptop/Desktop)
+        1024: {
+            slidesPerView: 3,
+        },
+        // When screen width is >= 1440px (Large screen)
+        1440: {
+            slidesPerView: 4,
+        }
+    }
 });
 
 
@@ -49,25 +67,25 @@ var swiper = new Swiper(".mySwiper", {
 let navToggle = document.querySelector('.menu-toggle');
 let navLi = document.querySelectorAll('.nav-links ul li');
 
-navToggle.addEventListener('click',()=>{
+navToggle.addEventListener('click', () => {
     console.log("clicked")
     let navshow = document.querySelector('.nav-links');
-    if(navshow.style.display === "none" || navshow.style.display === ""){
+    if (navshow.style.display === "none" || navshow.style.display === "") {
         navshow.style.display = "flex";
         navAnimation();
-    }else{
+    } else {
         navshow.style.display = "none";
-        navLi.forEach((li,idx)=>{       
-            li.style.transform = `translateX(-100%)`; 
+        navLi.forEach((li, idx) => {
+            li.style.transform = `translateX(-100%)`;
             li.style.transition = `all 0.5s ease`;
-    });
+        });
     }
 });
 
 function navAnimation() {
-    navLi.forEach((li,idx)=>{       
+    navLi.forEach((li, idx) => {
         setTimeout(() => {
-            li.style.transform = `translateX(0px)`; 
+            li.style.transform = `translateX(0px)`;
             li.style.transition = `all 0.5s ease`;
         }, idx * 200);
     });
